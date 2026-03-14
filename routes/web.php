2cc4bb7456b7e6b1cc8f28b918429cfa91072ec2
3312501034.Robi@students.polibatam.ac.id <MY_NAME@example.com>
2026-03-14 15:13:18 +0700

    Praktikum MVC Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListBarangController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome', function () {
+    return "Welcome";
 });
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID: " . $id;
+});
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+
+});
+
+//Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+//    return view('list_barang', [
+//        'id' => $id,
+//        'nama' => $nama
+//    ]);
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
